0525587c532692fb225a5f9423352bcb2e2a639f
rami syriani <71677294+syrianii@users.noreply.github.com>
2024-01-01 17:27:19 +0300

    Pushing Day 1

---
+++ day_1_project.py
@@ -0,0 +1,5 @@
+print("Hello Dear Users!")
+city_name = input("Which city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city_name + pet_name
+print("Your band name could be:\n" + band_name)
---
+++ variable_switching.py
@@ -0,0 +1,13 @@
+### Variable Switching using python
+
+#Reading the variables from the user
+a = input("a:")
+b = input("b:")
+
+#doing a switch using a third variable
+t = a
+a = b
+b = t
+
+print("a = ",a)
+print("b = ",b)
